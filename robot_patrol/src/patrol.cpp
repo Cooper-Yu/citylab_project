f8b349483dae77057df0e47dd684e53ed6a48d24
Yu Ge <liuzi9240@gmail.com>
2026-04-22 04:53:10 +0000

    Add LaserScan subscriber, cmd_vel publisher and control timer

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -18,13 +18,28 @@ public:
       best_index_(-1),
       obstacle_ahead_(false)
     {
-        
+        auto qos = rclcpp::QoS(10).reliability(rclcpp::ReliabilityPolicy::Reliable);  
+        laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "fastbot_1/scan",
+            qos,
+            std::bind(&Patrol::laserscan_callback, this, std::placeholders::_1)
+        );
+        twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("fastbot_1/cmd_vel", 10);
+
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100),
+            std::bind(&Patrol::control_loop, this)
+        );
 
         RCLCPP_INFO(this->get_logger(), "Patrol node started.");
     
     }
 
 private:
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr  twist_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
     double direction_;
     int best_index_;
     bool obstacle_ahead_;
